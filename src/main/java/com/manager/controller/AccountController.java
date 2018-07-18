e8fffaa0088881b982d0fb7ff1bd49283a18b824
wudeji <YW0916@YW-01B-0523.corp.yaowan.com>
2018-07-18 15:59:50 +0800

    restfull

--- src/main/java/com/manager/controller/AccountController.java
+++ src/main/java/com/manager/controller/AccountController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.manager.model.Account;
 import com.manager.service.AccountService;
@@ -16,28 +18,28 @@ import com.manager.shiro.annotations.ShiroDescription;
  * @author 吴德基  
 	created at 2018年7月12日 下午4:05:04
  */
-@Controller
+@RestController
 @RequestMapping("/account")
 public class AccountController {
 	
 	@Autowired
 	private AccountService accountService;
 
-	@RequestMapping("/list")
+	@RequestMapping(value="/list",method=RequestMethod.GET)
 	public String getAllAccount(Model model){
 		List<Account> list = accountService.getAllAccount();
 		model.addAttribute("list", list);
 		return "account/list";
 	}
 	
-	@RequestMapping("/createIndex")
+	@RequestMapping(value="/createIndex",method=RequestMethod.GET)
 	@ShiroDescription(name="创建账号首页")
 	public String createAccount(){
 		return "account/create";
 	}
 	
 	
-	@RequestMapping("/create")
+	@RequestMapping(value="/create",method=RequestMethod.POST)
 	@ShiroDescription(name="创建账号")
 	public String createAccount(Model model,String name,String password){
 		boolean value = accountService.createAccount(name, password);
